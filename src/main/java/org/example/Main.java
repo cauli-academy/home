5969d6eb9d8c8287e68b530e9f1ac60a4d2103da
cauli-academy <szkolenia@cauli-academy.com>
2024-11-23 14:08:00 +0100

    cleaning

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,6 @@ package org.example;
 
 import org.example.rooms.Room;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Witaj w grze");
